52ac778ec7fe843e2f5bfb21e2383deafba772e5
Alexey Parilov <alexeyparilov@rfc1918.private.ip.localhost>
2016-11-08 16:37:34 +0300

    third completed

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,7 +1,51 @@
 class CardsController < ApplicationController
-	
+
   def index
     @cards = Card.all
+  end
+
+  def new
+    @status = 'hide'
+  end
+
+  def edit
+    @status = 'hide'
+    @card = Card.find(params[:id])
+  end
+
+  def update
+    @card = Card.find(params[:id])
+    @card.update(card_params)
+     if @card.save
+      redirect_to cards_path
+    else
+      @message = 'Bce поля обязательны для заполнения, оригинал и перевод должны отличаться'
+      @status = 'show'
+      render 'edit'
+    end
+  end
+
+  def create
+    @card = Card.new(card_params)
+    @card.update(:review_date => $date_mark)
+     if @card.save
+      redirect_to cards_path
+    else
+      @message = 'Bce поля обязательны для заполнения, оригинал и перевод должны отличаться'
+      @status = 'show'
+      render 'new'
+    end
+  end
+
+  def destroy
+    @card = Card.find(params[:id])
+    @card.destroy
+    redirect_to cards_path
+  end
+
+  private
+  def card_params
+    params.require(:card).permit(:original_text, :translated_text)
   end
 
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,17 @@
 class Card < ApplicationRecord
+
+  after_initialize do
+    $date_mark = Time.now + 259200
+  end
+  
+  validates :original_text, :translated_text, :review_date, presence: true
+
+  validate :origin_notequal_translation
+
+  def origin_notequal_translation
+    if original_text.casecmp(translated_text) == 0
+      errors.add(:original_text)
+    end
+  end
+
 end
