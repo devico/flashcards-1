2ce2e52162f42e41663f1514b7e915fdc2699286
Alexey Parilov <alexeyparilov@rfc1918.private.ip.localhost>
2016-11-11 13:58:41 +0300

    completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'nokogiri'
+
+@date_rec = Time.now + 259_200
+@initial = "http://www.languagedaily.com/learn-german/vocabulary/common-german-words"
+@set = Nokogiri::HTML(open(@initial))
+@links = @set.xpath("//div[@class='jsn-article-content']/ul/li/a").map{|link| link['href']}
+@links << '/learn-german/vocabulary/common-german-words'
+@links_count = @links.length - 1
+
+(0..@links_count).each do |x|
+  @initial_inner = "http://www.languagedaily.com" + @links[x]
+  @set_inner = Nokogiri::HTML(open(@initial_inner))
+  @original = @set_inner.xpath("//tr[@class='rowA' or @class='rowB']/td[2]").map{|td| td.text}
+  @translation = @set_inner.xpath("//tr[@class='rowA' or @class='rowB']/td[3]").map{|td| td.text}
+  @count = @original.length
+    (0..@count).each do |i|
+        if @translation[i].to_s.length > 10
+          @original[i] = @original[i].gsub("'"){"\\'"}
+          @translation[i] = @translation[i].gsub("'"){"\\'"}
+          Card.create(original_text: @original[i], translated_text: @translation[i], review_date: @date_rec)
+        end
+      end
+end
