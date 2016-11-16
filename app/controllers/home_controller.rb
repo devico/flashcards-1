840358c7654c9ba8331355de3982d05226eb577f
Alexey Parilov <alexeyparilov@rfc1918.private.ip.localhost>
2016-11-16 16:32:37 +0300

    5.2 + hound

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,13 @@
 class HomeController < ApplicationController
-
   def index
     @card = Card.review_date_earlier_or_equal.pick_rand
   end
 
   def check
-        result = CheckTranslation.call(
+    result = CheckTranslation.call(
       users: params[:home][:user_text],
       card_id: params[:home][:id_code]
-    )
-
+      )
     flash[result.key] = result.notice
     redirect_to action: "index"
   end
