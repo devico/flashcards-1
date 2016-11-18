3379bceab9ab66c1b0223729d1a3b7a843af3532
Alexey Parilov <alexeyparilov@MacBook-Air-Alexey.local>
2016-11-19 01:28:13 +0300

    7 tests added

---
+++ spec/features/translation_check_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+RSpec.feature "Translation check" do
+  before do
+    record_time = 3.days.ago
+    Time.stub(:now) { record_time }
+    card_test = create(:card, original_text: 'Correct translation', translated_text: 'Text to check') 
+  end
+  scenario "User inputs correct translation and checks the result" do
+    visit "/"
+    fill_in "Перевод", :with => 'Correct translation'
+    click_button "Проверить"
+    expect(page).to have_text("Все верно")
+  end
+  scenario "User inputs wrong translation and checks the result" do
+    visit "/"
+    fill_in "Перевод", :with => 'Wrong translation'
+    click_button "Проверить"
+    expect(page).to have_text("Ошибка")
+  end
+end
