fcb2ece0ee6c15ac26a6f3c9a5ad10ffb9be68f1
Alexey Parilov <alexeyparilov@rfc1918.private.ip.localhost>
2016-11-18 14:57:55 +0300

    6 test for cut_val hound

--- spec/card_custom_val_spec.rb
+++ spec/card_custom_val_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 describe Card do
   context 'before saved' do
     it 'shall return false if texts are equal regardless registr' do
-     expect(Card.new(original_text: 'Test', translated_text: 'tEst')).not_to be_valid
+      expect(Card.new(original_text: 'Test', translated_text: 'tEst')).not_to be_valid
     end
   end
 end
