528eeded5b29d2d4be916511e7e6a70f51327ce9
Alexey Parilov <alexey.parilov@gmail.com>
2016-11-20 00:48:24 +0300

    change check_transl

--- app/interactors/check_translation.rb
+++ app/interactors/check_translation.rb
@@ -4,7 +4,7 @@ def call
   card_check = Card.find_by(id: context.card_id)
   if card_check.original_text.strip == context.users.strip
     card_check.update(review_date: 3.days.since)
-    context.notice = 'Все верно! Для закрепления повторим через три дня'
+    context.notice = 'Все верно! Для закрепления повторим через 3 дня'
   else
     context.notice ='Ошибка, неправильный перевод'
   end
