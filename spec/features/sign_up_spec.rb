b95b12e314999898579d7474a71e8cef445e2ba8
Alexey Parilov <alexeyparilov@MacBook-Air-Alexey.local>
2016-12-11 01:13:51 +0300

    done before hound

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -1,12 +1,16 @@
 require "rails_helper"
 RSpec.feature "Sign up check" do
-  scenario "User signs up and creates card" do
+  scenario "User signs up creates deck and card" do
     visit "/sign_up"
     fill_in "Email:", with: '123@123.ru'
     fill_in "Пароль:", with: 'secret'
     fill_in "Подтвердите пароль:", with: 'secret'
     click_button "Сохранить учетную запись!"
+    visit "/decks"
+    fill_in "Название колоды:", with: 'Testing'
+    click_button "Создать колоду"
     visit "/cards/new"
+    select 'Testing', from: 'Выберите колоду'
     fill_in "Оригинальный текст:", with: 'My original text'
     fill_in "Перевод:", with: 'My translation'
     click_button "Сохранить карточку"
