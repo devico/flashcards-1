6ad159fd9e66715506bf21b9ff0982c71624c3ba
Alexey Parilov <alexeyparilov@MacBook-Air-Alexey.local>
2016-12-12 00:31:15 +0300

    final fixes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     result = NominateCards.call(
       user: current_user
-      )
+    )
     @status = result.status
     @card = result.card
   end
@@ -11,7 +11,7 @@ class HomeController < ApplicationController
     result = CheckTranslation.call(
       users: params[:home][:user_text],
       card_id: params[:home][:id_code]
-      )
+    )
     flash[:notice] = result.notice
     redirect_to action: "index"
   end
--- app/interactors/nominate_cards.rb
+++ app/interactors/nominate_cards.rb
@@ -4,10 +4,10 @@ def call
   if Deck.exists?(id: context.user.deck_id)
     deck = Deck.find(context.user.deck_id)
     context.status = 'Карточки из колоды ' + deck.name
-    context.card = Card.from_active_deck(context.user).random.first
+    context.card = Card.from_active_deck(context.user).review_date_earlier_or_equal.random.first
   else
     context.status = 'Колода не выбрана, показываем случайные карточки'
-    context.card = Card.belongs_to_current_user(context.user).random.first
+    context.card = Card.belongs_to_current_user(context.user).review_date_earlier_or_equal.random.first
   end
 end
 end
