c33fc0fd8a19655785d5aa36943f3ac96677d2b7
Alexey <alexey.parilov@gmail.com>
2016-12-14 09:06:20 +0000

    review date set if params nil

--- app/models/card.rb
+++ app/models/card.rb
@@ -6,15 +6,17 @@ class Card < ApplicationRecord
   scope :from_active_deck, lambda {|current_user| where(deck_id: current_user.deck_id) }
   scope :belongs_to_current_user, lambda {|current_user| where(user_id: current_user.id) }
   scope :random, -> {order('RANDOM()').limit(1) }
-  #before_create :set_review_date
+  before_create :set_review_date
 
   def self.pick_rand
     order('RANDOM()').first
   end
   
-  #def set_review_date
-    #self.review_date = 3.days.since
-  #end
+  def set_review_date
+    if :review_date.nil?
+      self.review_date = 3.days.since
+    end
+  end
 
   validates :original_text, :translated_text, :deck_id, presence: true
   validate :origin_notequal_translation
